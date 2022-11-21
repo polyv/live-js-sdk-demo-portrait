4af0140e10fcbbc55d1b31f4f68b68a259b1de55
twt-0308 <3465362934@qq.com>
2022-11-21 18:21:08 +0800

    新增注释

--- src/assets/store/webview.js
+++ src/assets/store/webview.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 
+// 小型 Vuex，全局状态管理
 export const webviewStore = Vue.observable({
-  isSmallWindow: false,
-  isPortrait: false
+  isSmallWindow: false, // 是否开启了小窗口模式
+  isPortrait: false // 是否为竖屏比例
 });
