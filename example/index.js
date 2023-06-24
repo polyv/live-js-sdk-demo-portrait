34ee5a5171ac9800166082e1f857d7fa41a6863e
sxuan <609311490@qq.com>
2023-06-24 12:49:20 +0800

    fix 去除key

--- example/index.js
+++ example/index.js
@@ -3,8 +3,8 @@ import VConsole from 'vconsole';
 
 new PolyvPortraitWatch({
   el: document.getElementById('portrait-view'),
-  appId: 'g1zd4otbpz',
-  appSecret: '9a2912c80afa49b6b7a0fb94184940ef',
+  appId: '',
+  appSecret: '',
   channelId: '4057466',
   user: {
     userId: '123456',
