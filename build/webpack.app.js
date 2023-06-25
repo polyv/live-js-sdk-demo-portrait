8ec5436691e830ede155492045da8bf647956b45
H-zk <hzkzero16@gmail.com>
2023-06-25 10:20:08 +0800

    chore: 支持通过 build:app 命令来构建 example

---
+++ build/webpack.app.js
@@ -0,0 +1,32 @@
+/**
+ * @file 用来生产构建 example
+ */
+
+const path = require('path');
+const merge = require('webpack-merge');
+const common = require('./webpack.base.js');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = merge(common, {
+  mode: 'production',
+  entry: {
+    index: './example/index.js'
+  },
+  output: {
+    path: path.resolve(__dirname, '../.dist'),
+    publicPath: './',
+    filename: 'polyv-portrait-watch.min.js',
+    libraryTarget: 'umd',
+    umdNamedDefine: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './example/index.html',
+      inject: true
+    }),
+    new MiniCssExtractPlugin({
+      filename: '/polyv-portrait-watch.min.css'
+    }),
+  ],
+});
